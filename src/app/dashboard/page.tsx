c23ef6a8f5cd47d6d2cba4b5baf5f98e532bad30
Juanca <riberojuanca@gmail.com>
2024-09-04 00:49:38 -0300

    feat: add  conditional rendering for user image in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import LogoutButton from "@/components/LogOutButton";
 import prisma from "@/lib/db";
+import Image from "next/image";
 
 async function dashboard() {
   const session = await auth();
@@ -12,15 +13,31 @@ async function dashboard() {
       email: session.user.email as string,
     },
   });
-  if (!user) return <div>Usuario no encontrado</div>;
+  if (!user) return <div>No estas logueado</div>;
 
   return (
-    <div>
-      <div>
-        <h1>Hola, {user.name} 👋🏽</h1>
-      </div>
-      <LogoutButton />
-    </div>
+    <article className="p-4">
+      {session.user.image !== null ? (
+        <div className="flex items-center gap-4">
+          <img
+            className="w-20 h-20 rounded-md"
+            src={`${session.user.image}`}
+            alt=""
+          ></img>
+          <div>
+            <h1>Hola, {user.name} 👋🏽</h1>
+            <LogoutButton />
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <div>
+            <h1>Hola, {user.name} 👋🏽</h1>
+            <LogoutButton />
+          </div>
+        </div>
+      )}
+    </article>
   );
 }
 
