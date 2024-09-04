2bf7006af46be47df9beccf5b87cec18153b6cfc
Juanca <riberojuanca@gmail.com>
2024-09-04 00:54:00 -0300

    feat: add Github login button and post-login rendering

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,36 @@
 import LoginForm from "@/components/loginForm";
+import { auth, signIn, signOut } from "@/auth";
+import LogoutButton from "@/components/LogOutButton";
+import Image from "next/image";
 
-function loginPage() {
+async function loginPage() {
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return (
+      <article>
+        <div>loginPage</div>
+        <LoginForm />
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github", {
+              redirectTo: "/dashboard",
+            });
+          }}
+        >
+          <button type="submit">Iniciar con Github</button>
+        </form>
+      </article>
+    );
+  }
+  console.log(session);
   return (
-    <section>
-      <div>loginPage</div>
-      <LoginForm />
-    </section>
+    <div>
+      <LogoutButton />
+      <p>{session.user.name}</p>
+      <img src={session.user.image as string} alt="user image" />
+    </div>
   );
 }
 
