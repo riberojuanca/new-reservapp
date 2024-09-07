0560f60a6fd5e6fcd044de038e0c7b6ebc302cbe
Juanca <riberojuanca@gmail.com>
2024-09-07 11:23:50 -0300

    update: update LogOut button

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -9,7 +9,13 @@ const LogoutButton = () => {
     });
   };
 
-  return <button onClick={handleClick}>Logout</button>;
+  return (
+    <button className="uppercase" onClick={handleClick}>
+      <small className="bg-stone-800 flex items-center justify-center text-stone-400  rounded-sm font-bold p-2">
+        Logout
+      </small>
+    </button>
+  );
 };
 
 export default LogoutButton;
